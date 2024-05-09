20e29d62951c6c6b917ba4062cd9de8e9605b652
Sathiya@1998 <sathiyavenkatachalam258@gmail.com>
2024-05-09 14:38:34 +0530

    2 commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementById("root").innerText = "10"
+    setTimeout(() => {
+        document.getElementById("root").innerText = "9"
+        setTimeout(() => {
+            document.getElementById("root").innerText = "8"
+            setTimeout(() => {
+                document.getElementById("root").innerText = "7"
+                setTimeout(() => {
+                    document.getElementById("root").innerText = "6"
+                    setTimeout(() => {
+                        document.getElementById("root").innerText = "5"
+                        setTimeout(() => {
+                            document.getElementById("root").innerText = "4"
+                            setTimeout(() => {
+                                document.getElementById("root").innerText = "3"
+                                setTimeout(() => {
+                                    document.getElementById("root").innerText = "2"
+                                    setTimeout(() => {
+                                        document.getElementById("root").innerText = "1"
+                                        setTimeout(() => {
+                                            document.getElementById("root").innerText = "Happy Independence Day"
+                                        },1000)
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, );
